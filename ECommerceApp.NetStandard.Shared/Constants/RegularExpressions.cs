8be4da410e9a9edda1c94a32a7999b3dfac7640c
SamanthaNeilen <33734383+SamanthaNeilen@users.noreply.github.com>
2017-12-23 09:23:50 +0100

    Completed validation rules for customer page

---
+++ ECommerceApp.NetStandard.Shared/Constants/RegularExpressions.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerceApp.NetStandard.Shared.Constants
+{
+    public static class RegularExpressions
+    {
+        public const string EmailRegularExpression = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,6})+)$";
+        public const string DutchZipCodeRegularExpression = "^[1-9][0-9]{3}([A-Z]{2})$";
+        public const string DutchPhoneNumberRegularExpression = "^(((0)[1-9]{2}[0-9][-]?[1-9][0-9]{5})|((\\+31|0|0031)[1-9][0-9][-]?[1-9][0-9]{6}))$";
+        public const string NumericRegularExpression = "^([0-9]*)$";
+    }
+}
--- ECommerceApp.NetStandard.Shared/Models/CustomerViewModel.cs
+++ ECommerceApp.NetStandard.Shared/Models/CustomerViewModel.cs
@@ -1,26 +1,25 @@
-﻿using ECommerceApp.NetFramework.Shared.Constants;
-using ECommerceApp.NetFramework.Shared.Resources;
+﻿using ECommerceApp.NetStandard.Shared.Constants;
+using ECommerceApp.NetStandard.Shared.Resources;
 using System.ComponentModel.DataAnnotations;
 
-namespace ECommerceApp.NetFramework.Shared.Models
+namespace ECommerceApp.NetStandard.Shared.Models
 {
-    //TODO labels!
     public class CustomerViewModel
     {
         public int Id { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.FieldRequired))]
-        [MaxLength(250, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.MaxLength))]
+        //[Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.FieldRequired))]
+        //[MaxLength(250, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.MaxLength))]
         public string Name { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.FieldRequired))]
-        [MaxLength(250, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.MaxLength))]
-        [RegularExpression(RegularExpressions.EmailRegularExpression, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.InvalidEmailAdress))]
+        //[Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.FieldRequired))]
+        //[MaxLength(250, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.MaxLength))]
+        //[RegularExpression(RegularExpressions.EmailRegularExpression, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.InvalidEmailAdress))]
         public string EmailAdress { get; set; }
 
-        [Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.FieldRequired))]
-        [MaxLength(20, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.MaxLength))]
-        [RegularExpression(RegularExpressions.DutchPhoneNumberRegularExpression, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.InvalidPhonenumber))]
+        //[Required(ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.FieldRequired))]
+        //[MaxLength(20, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.MaxLength))]
+        //[RegularExpression(RegularExpressions.DutchPhoneNumberRegularExpression, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.InvalidPhonenumber))]
         public string PhoneNumber { get; set; }
 
         [MaxLength(100, ErrorMessageResourceType = typeof(Messages), ErrorMessageResourceName = nameof(Messages.MaxLength))]
